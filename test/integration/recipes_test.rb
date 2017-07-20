000dd17b9d0b24b2adff5d53cbd2076dd3ff6f9a
Lorena Bento <lorenadgb@gmail.com>
2017-07-19 21:11:40 -0300

    Implement edit and delete

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -28,24 +28,28 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
     assert_match @chef.chefname, response.body
+    assert_select 'a[href=?]', edit_recipe_path(@recipe), text: 'Edit this recipe'
+    assert_select 'a[href=?]', recipe_path(@recipe), text: 'Delete this recipe'
   end
 
-  test 'create new valid recipe' do
+  test "create new valid recipe" do
     get new_recipe_path
     assert_template 'recipes/new'
-    name_of_recipe = 'chicken saute'
-    description_of_recipe = 'add chicken, add vegetables, cook for 20 minutes, serve delicious meal'
-    assert_difference 'Receipe.count', 1 do
-      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe }}
+    name_of_recipe = "chicken saute"
+    description_of_recipe = "add chicken, add vegetables, cook for 20 minutes, serve delicious meal"
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: { name: name_of_recipe, description: description_of_recipe}}
     end
-    follow_redirect
+    follow_redirect!
+    assert_match name_of_recipe.capitalize, response.body
+    assert_match description_of_recipe, response.body
   end
 
-  test 'reject invalid recipe submissions' do
+  test "reject invalid recipe submissions" do
     get new_recipe_path
     assert_template 'recipes/new'
     assert_no_difference 'Recipe.count' do
-      post recipes_path, params: { recipe: { name: ' ', description: ' ' } }
+      post recipes_path, params: { recipe: { name: " ", description: " " } }
     end
     assert_template 'recipes/new'
     assert_select 'h2.panel-title'
