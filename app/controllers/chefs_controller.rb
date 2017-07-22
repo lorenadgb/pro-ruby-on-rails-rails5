a6ff900ed67d93dac98784898558dd4da0eb9511
Lorena Bento <lorenadgb@gmail.com>
2017-07-21 23:09:10 -0300

    Add edit action with tests

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -14,8 +14,22 @@ class ChefsController < ApplicationController
     end
   end
 
+  def edit
+    @chef = Chef.find(params[:id])
+  end
+
   def show
     @chef = Chef.find(params[:id])
+  end
+
+  def update
+    @chef = Chef.find(params[:id])
+    if @chef.update(chef_params)
+      flash[:success] = 'Your account was updated successfully'
+      redirect_to @chef
+    else
+      render 'edit'
+    end
   end
 
   private
---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @chef = Chef.create!(chefname: 'lorena', email: 'lorenadgb@gmail.com', password: "password", password_confirmation: "password")
+  end
+
+  test 'reject an invalid edit' do
+    get edit_chef_path(@chef)
+    patch chef_path(@chef), params: { chef: { chefname: ' ', email: 'lorena@example.com' } }
+    assert_template 'chefs/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
+  test 'accept valid signup' do
+    get edit_chef_path(@chef)
+    patch chef_path(@chef), params: { chef: { chefname: 'lorena bento', email: 'lorena@example.com' } }
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match 'lorena bento', @chef.chefname
+    assert_match 'lorena@example.com', @chef.email
+  end
+
+end
