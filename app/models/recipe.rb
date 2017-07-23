5e04ef2b3e847714698ef1896533664a39fe1ac5
Lorena Bento <lorenadgb@gmail.com>
2017-07-23 00:08:36 -0300

    Add chefs destroy and tests

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   validates :name, :chef_id, presence: true
   validates :description, presence: true, length: { minimum: 5, maximum: 500 }
-  validates :cehf_id, presence: true
+  validates :chef_id, presence: true
 
   belongs_to :chef
 
